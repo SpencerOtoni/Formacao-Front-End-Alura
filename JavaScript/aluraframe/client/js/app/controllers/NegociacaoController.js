74cfb1aa5845c0181a8c77ddc06662b28d3a4e75
Spencer Otoni de Souza <sspencerotoni@gmail.com>
2021-01-09 21:35:07 -0300

    Class NegociacaoController

---
+++ JavaScript/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,9 @@
+class NegociacaoController{
+    adiciona(event){
+        event.preventDefault()
+        let $ = document.querySelector.bind(document);
+        let inputData =  $('#data');
+        let inputQuantidade = $('#quantidade');
+        let inputValor = $('#valor');
+    }
+}
