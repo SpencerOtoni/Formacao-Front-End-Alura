a302a3f4b3f9789aad34c84f6c78c45cba3f5ad7
Spencer Otoni de Souza <sspencerotoni@gmail.com>
2021-01-11 22:40:18 -0300

    Implementando Class ListaNegociacoes

--- JavaScript/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JavaScript/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -6,19 +6,33 @@ class NegociacaoController{
         this._inputData =  $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacao = new ListaNegociacoes();
 
     }
-    
+
     adiciona(event){
         event.preventDefault()
 
-        let negociacao = new Negociacao(
+        this._listaNegociacao.adiciona(this._criaNegociacao())
+        this._limpaFormulario()        
+    }
+
+    _criaNegociacao(){
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         )
-        
-        let diaMesAno = DateHelper.dataParaTexto(negociacao.data)
-        console.log(negociacao.data, diaMesAno)
     }
+
+    _limpaFormulario(){
+        this._inputData.value =  '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+
+        this._inputData.focus()
+    }
+
+    /* let diaMesAno = DateHelper.dataParaTexto(negociacao.data)
+    console.log(negociacao.data, diaMesAno) */
 }
